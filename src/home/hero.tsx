25edbc19af6316704642b70b29555d92c0bb4d78
rohitkesharwani9 <rohit.kesharwani9@gmail.com>
2025-06-20 12:50:17 +0530

    upload file

---
+++ src/home/hero.tsx
@@ -0,0 +1,92 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+import styled from 'styled-components';
+
+const HeroHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 20px;
+
+  & > * {
+    flex-basis: 50%;
+  }
+
+  h3 {
+    margin-bottom: -10px;
+    font-weight: normal;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  .cta-holder {
+    display: flex;
+    justify-content: center;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+
+    h3 {
+      text-align: start;
+    }
+
+    h1 {
+      font-size: 2.5rem;
+      text-align: start;
+    }
+
+    .cta-holder {
+      justify-content: flex-start;
+    }
+  }
+`;
+
+const CTA = styled.span`
+  padding: 1.0rem 2rem;
+  background: #6c63ff;
+  color: white;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 2.2rem;
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+    padding: 0.8rem 1.5rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.2rem;
+    padding: 0.6rem 1.2rem;
+  }
+`;
+
+const Hero: NextPage = () => {
+  return (
+    <HeroHolder>
+      <div>
+        <h3>Create and Download in PDF Format</h3>
+        <h1>
+          Free Job Biodata Maker.<br />
+          Click the "<b>Start Making Your Bio</b>" button below to create your biodata for a job
+        </h1>
+        <div className="cta-holder">
+          <Link href="/editor">
+            <a>
+              <CTA>Start Making Your Bio</CTA>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <Image src="/hiring.svg" alt="hiring" width="300px" height="300px" priority />
+    </HeroHolder>
+  );
+};
+
+export default Hero;
---
+++ src/pages/editor.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Resume } from 'src/core/containers/Resume';
+import { Sidebar } from 'src/core/containers/Sidebar';
+import { LeftNav } from 'src/core/containers/LeftNav';
+import { FlexHC } from 'src/styles/styles';
+
+const Editor: NextPage = () => {
+  return (
+    <FlexHC>
+      <Head>
+        <title>Biodata Maker For Job Editor Page</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/draggar.css"></link>
+        {/* Google Analytics Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q6S926GZYM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Q6S926GZYM');
+            `,
+          }}
+        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2305974348753248"
+     crossOrigin="anonymous"></script>
+      </Head>
+      <div id="navbar1">
+          <span>
+            <b>Biodata Maker For Job</b>
+          </span>
+        </div>
+        <br />
+      <LeftNav />
+      <Resume />
+      <Sidebar />
+    </FlexHC>
+  );
+};
+
+export default Editor;
